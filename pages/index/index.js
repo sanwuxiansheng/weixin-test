e281ef402ec229719f530fcea6fc05cf445a1067
chenggongcai <1197063758@qq.com>
2019-07-23 21:09:16 +0800

    创建微信小程序初始界面以及跳转到第二个页面并实现第二个页面的轮播图

---
+++ pages/index/index.js
@@ -0,0 +1,76 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+  gotoList() {
+    // wx.navigateTo(Object object) 跳到指定界面后可以返回到之前的界面
+    // wx.navigateTo({
+    //   url: '/pages/list/list',
+    // })
+    // wx.redirectTo()  跳到指定界面后不能返回到之前的界面
+    wx.redirectTo({
+      url: '/pages/list/list',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
